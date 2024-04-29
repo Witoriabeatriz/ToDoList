571521e392c945c2d2ec0717337a0be56727e1da
Witoriabeatriz <witoria.santos@alunoescolassociais.org.br>
2024-04-29 09:35:43 -0300

    iniciando

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gerenciador de tarefas</title>
+</head>
+<body>
+    
+<div class="container">
+    <div class = "header">
+        <h1>Gerenciador de tarefas</h1>
+    </div>    
+    <div class ="form">
+        <form action="" method="get">
+            <label for="task_name"></label>
+            <input type="text" name="task_name" placehorder="Nome da tarefa">
+            <button type="submit"> Cadastrar</button>
+        </form>
+    </div>
+    <div class ="separator">
+    </div>
+    <div class ="list-tasks">
+        <ul>
+            <li> Tarefa 1 </li>
+            <li> Tarefa 2 </li>
+            <li> Tarefa 3 </li>
+        </ul>
+    </div>
+    <div class ="footer">
+        <p>Desenvolvimento por @Witória beatriz</p>
+    </div>
+</div>
+
+
+</body>
+</html>
