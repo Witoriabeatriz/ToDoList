b9bebef80909bed9918f4c2cf2ebefb9b3b63d47
Witoriabeatriz <witoria.santos@alunoescolassociais.org.br>
2024-04-29 11:56:38 -0300

    Finalizando

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # ToDoList
-ToDoList
+
+Este repositório contém uma aplicação simples de gerenciador de tarefas desenvolvida em PHP, HTML5 e CSS3, conforme tutorial do canal Monolito PHP.
+
+## Tutorial de Construção
+
+O tutorial para construir esta aplicação pode ser encontrado no vídeo [Projeto - Gerenciador de Tarefas](link_para_o_vídeo.mp4), disponibilizado pelo canal Monolito PHP. O vídeo é um guia passo a passo sem áudio, permitindo que você acompanhe e reproduza o código apresentado.
+
+## Créditos
+
+O código desta aplicação foi desenvolvido pelo criador do canal Monolito PHP. Por favor, assegure-se de dar crédito ao autor mencionado acima ao utilizar ou referenciar este código.
+
+## Recursos PHP Utilizados
+
+Durante a construção desta aplicação, o autor do vídeo utilizou diversos recursos da linguagem PHP. Abaixo está uma breve explicação de alguns dos principais recursos mencionados:
+
+- **isset()**: Função utilizada para verificar se uma variável está definida e não é null.
+- **$_SESSION**: Variável superglobal utilizada para armazenar dados de sessão do usuário.
+- **foreach**: Estrutura de controle utilizada para iterar sobre arrays.
+- **session_start()**: Função utilizada para iniciar uma nova sessão ou continuar a sessão existente.
+- **array()**: Função utilizada para criar arrays.
+- **$_GET**: Variável superglobal utilizada para coletar dados enviados através do método GET em formulários.
+- **array_push()**: Função utilizada para adicionar um ou mais elementos ao final de um array.
+- **var_dump()**: Função utilizada para exibir informações sobre uma variável.
+- **unset()**: Função utilizada para destruir as variáveis especificadas.
+
+Para mais detalhes sobre esses recursos e outros recursos PHP utilizados no projeto, consulte a documentação oficial do PHP em [php.net](https://www.php.net/manual/).
+
+## LinkedIn
+
--- index.php
+++ index.php
@@ -1,39 +1,58 @@
+<?php
+session_start();
+if (!isset($_SESSION['tasks'])) {
+    $_SESSION['tasks'] = array();
+}
+if (isset($_GET['task_name'])) {
+    array_push($_SESSION['tasks'], $_GET['task_name']);
+    unset($_GET['task_name']);
+}
+if (isset($_GET['clear'])) {
+    unset($_SESSION['tasks']);
+}
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Gerenciador de Tarefas</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
     <link rel="stylesheet" href="style.css">
-    <title>Gerenciador de tarefas</title>
+    <script src='main.js'></script>
 </head>
 <body>
-    
 <div class="container">
-    <div class = "header">
+    <div class="header">
         <h1>Gerenciador de tarefas</h1>
-    </div>    
-    <div class ="form">
+    </div>
+    <div class="form">
         <form action="" method="get">
-            <label for="task_name">Tarefa:</label>
+            <label for="task_name">Tarefa: </label>
             <input type="text" name="task_name" placeholder="Nome da tarefa">
-            <button type="submit"> Cadastrar</button>
+            <button type="submit">Cadastrar</button>
         </form>
     </div>
-    <div class ="separator">
-    </div>
-    <div class ="list-tasks">
+    <div class="separator"></div>
+    <div class="list-tasks">
         <ul>
-            <li> Tarefa 1 </li>
-            <li> Tarefa 2 </li>
-            <li> Tarefa 3 </li>
+            <?php
+            if (isset($_SESSION['tasks'])) {
+                foreach ($_SESSION['tasks'] as $key => $task) {
+                    echo "<li>$task</li>";
+                }
+            }
+            ?>
         </ul>
     </div>
-    <div class ="footer">
-        <p>Desenvolvimento por @Witória beatriz</p>
+    <form action="" method="get">
+        <input type="hidden" name="clear" value="clear">
+        <button type="submit" class="btn-clear">Limpar tarefas</button>
+    </form>
+    <br>
+    <div class="footer">
+        <p>Desenvolvido por @Witória Beatriz</p>
     </div>
 </div>
-
-
 </body>
 </html>
